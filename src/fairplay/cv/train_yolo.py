d25b13adf36a2a982f14284482f79edbff771855
Nils Persson <Imperssonator@users.noreply.github.com>
2025-10-19 21:05:52 -0400

    try again on bbox plotting

--- src/fairplay/cv/train_yolo.py
+++ src/fairplay/cv/train_yolo.py
@@ -107,8 +107,8 @@ def on_new_best_model(trainer): # This callback is only triggered if validation 
         # The model is already on the correct device
         # The image tensor from the dataloader might be uint8, so convert to float and normalize
         img_tensor = batch['img'].to(trainer.device).float() / 255.0
-        # Use the validator's model to get Results objects with the .plot() method
-        preds = trainer.validator.model(img_tensor)
+        # Use the model's predict() method to get Results objects
+        preds = trainer.model.predict(img_tensor, verbose=False)
 
         # Plot and save up to 4 images from the batch
         num_to_visualize = min(len(preds), 4)
